ccba05f5901df467188005b61d451e5af30b2103
mitchtheuvenet <mitch.theuvenet@gmail.com>
2021-03-27 01:16:41 +0100

    added logo to homepage

--- views/home.php
+++ views/home.php
@@ -6,5 +6,8 @@ $this->title = 'Home';
 
 <div class="col-md-4 offset-md-4 text-center">
     <h1>Home</h1>
-    <h3>Welcome, <?= $name ?>!</h3>
+    <p class="lead">Welcome, <?= $name ?>!</p>
+    <a href="/" tabindex="-1">
+        <img src="images/logo.png" class="img-fluid" alt="Logo">
+    </a>
 </div>
