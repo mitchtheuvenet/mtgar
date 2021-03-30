8100977da0352f190a5cb9083d29fc1f924da58a
mitchtheuvenet <mitch.theuvenet@gmail.com>
2021-03-30 17:33:59 +0200

    updated verifcations table 2

--- migrations/m0002_verifications.php
+++ migrations/m0002_verifications.php
@@ -11,8 +11,8 @@ class m0002_verifications extends Migration {
                 `user` INT NOT NULL,
                 `type` TINYINT NOT NULL,
                 `code` CHAR(32) NOT NULL,
-                `used` BIT DEFAULT 0,
-                `expired` BIT DEFAULT 0,
+                `used` BIT DEFAULT 0 NOT NULL,
+                `expired` BIT DEFAULT 0 NOT NULL,
                 `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
                 FOREIGN KEY (`user`) REFERENCES `users`(`id`)
