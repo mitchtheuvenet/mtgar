56600bcdee6a5cffeffd84946a78a89d1525016a
mitchtheuvenet <mitch.theuvenet@gmail.com>
2021-04-05 14:48:42 +0200

    implemented user status at login

--- models/Login.php
+++ models/Login.php
@@ -31,7 +31,7 @@ class Login extends Model {
     }
 
     public function logIn() {
-        $user = DbUser::findObject(['username' => $this->username]);
+        $user = DbUser::findObject(['username' => $this->username, 'status' => DbUser::STATUS_ACTIVE]);
 
         if (empty($user) || !password_verify($this->password, $user->password)) {
             return false;
