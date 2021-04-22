21d3a78faac5867e6545dfa7469e27e9d9d5d7c1
mitchtheuvenet <mitch.theuvenet@gmail.com>
2021-04-22 17:19:38 +0200

    added Mana util class

---
+++ utils/Mana.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\utils;
+
+abstract class Mana {
+
+    public const WHITE = 'W';
+    public const BLUE = 'U';
+    public const BLACK = 'B';
+    public const RED = 'R';
+    public const GREEN = 'G';
+    public const COLORLESS = 'C';
+
+}
