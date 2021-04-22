2580d316ce75047cd081028c9059bedfd48a3146
mitchtheuvenet <mitch.theuvenet@gmail.com>
2021-04-22 15:32:41 +0200

    implemented text-decoration-none

--- core/exceptions/InternalServerErrorException.php
+++ core/exceptions/InternalServerErrorException.php
@@ -5,6 +5,6 @@ namespace app\core\exceptions;
 class InternalServerErrorException extends \Exception {
 
     protected $code = 500;
-    protected $message = 'An unknown error has occurred. Please <a href="/contact" style="text-decoration:none;">contact the site administrators</a> if this error persists.';
+    protected $message = 'An unknown error has occurred. Please <a href="/contact" class="text-decoration-none">contact the site administrators</a> if this error persists.';
 
 }
