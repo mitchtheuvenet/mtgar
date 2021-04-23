4ce16727bd100fd53620183ff57d74a2208da4f5
mitchtheuvenet <mitch.theuvenet@gmail.com>
2021-04-24 01:30:09 +0200

    updated delete account confirm modal text

--- views/profile_delete_confirm.php
+++ views/profile_delete_confirm.php
@@ -30,7 +30,7 @@ $digits = $model::getCodeDigits();
                 <h5 class="modal-title" id="confirmModalLbl">Confirm account deletion</h5>
             </div>
             <div class="modal-body">
-                <p>Are you sure you want to delete your MTGAR account?</p>
+                <p>Are you sure you want to delete your MTGAR account? This action cannot be undone.</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
