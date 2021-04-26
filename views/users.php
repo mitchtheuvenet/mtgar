940397c79eaccffc929ee8b4f4e1383e36f0fd2d
mitchtheuvenet <mitch.theuvenet@gmail.com>
2021-04-26 12:45:39 +0200

    users nav buttons refactor 2

--- views/users.php
+++ views/users.php
@@ -12,9 +12,7 @@ $this->title = 'Users';
     <h1 class="text-center mb-4">Users</h1>
     <div class="d-flex justify-content-center mb-4">
         <a class="btn btn-primary<?= $index <= 0 ? ' disabled' : '' ?>" href="/users?index=<?= $index - 1; ?>" role="button"><i class="bi-arrow-left"></i></a>
-        <div class="align-self-center mx-3">
-            <p class="lead mb-0">Page <?= $index + 1; ?></p>
-        </div>
+        <p class="lead align-self-center mx-4 mb-0">Page <?= $index + 1; ?></p>
         <a class="btn btn-primary<?= $rowsLeft <= 0 ? ' disabled' : '' ?>" href="/users?index=<?= $index + 1; ?>" role="button"><i class="bi-arrow-right"></i></a>
     </div>
     <?php Table::print(['#', 'Username', 'E-mail address', 'Account status', 'Registered at'], DbUser::class, $users); ?>
