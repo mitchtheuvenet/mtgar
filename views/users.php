7bec4986ad9b9d971fe8b59412cbc5c6de7e4b2e
mitchtheuvenet <mitch.theuvenet@gmail.com>
2021-04-26 12:44:17 +0200

    users nav buttons refactor

--- views/users.php
+++ views/users.php
@@ -10,16 +10,12 @@ $this->title = 'Users';
 
 <div class="col-md-6 offset-md-3">
     <h1 class="text-center mb-4">Users</h1>
-    <div class="d-flex mb-4">
-        <div class="align-self-start mr-auto">
-            <a class="btn btn-primary<?= $index <= 0 ? ' disabled' : '' ?>" href="/users?index=<?= $index - 1; ?>" role="button"><i class="bi-arrow-left"></i></a>
-        </div>
-        <div class="align-self-center mx-auto">
+    <div class="d-flex justify-content-center mb-4">
+        <a class="btn btn-primary<?= $index <= 0 ? ' disabled' : '' ?>" href="/users?index=<?= $index - 1; ?>" role="button"><i class="bi-arrow-left"></i></a>
+        <div class="align-self-center mx-3">
             <p class="lead mb-0">Page <?= $index + 1; ?></p>
         </div>
-        <div class="align-self-end ml-auto">
-            <a class="btn btn-primary align-self-end<?= $rowsLeft <= 0 ? ' disabled' : '' ?>" href="/users?index=<?= $index + 1; ?>" role="button"><i class="bi-arrow-right"></i></a>
-        </div>
+        <a class="btn btn-primary<?= $rowsLeft <= 0 ? ' disabled' : '' ?>" href="/users?index=<?= $index + 1; ?>" role="button"><i class="bi-arrow-right"></i></a>
     </div>
     <?php Table::print(['#', 'Username', 'E-mail address', 'Account status', 'Registered at'], DbUser::class, $users); ?>
 </div>
