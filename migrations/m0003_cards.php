d2b6d9d522f99aeaf127ad378f401e8b0a7f8c13
mitchtheuvenet <mitch.theuvenet@gmail.com>
2021-05-15 14:28:15 +0200

    removed unused data from cards

--- migrations/m0003_cards.php
+++ migrations/m0003_cards.php
@@ -10,9 +10,6 @@ class m0003_cards extends Migration {
                 `id` INT AUTO_INCREMENT PRIMARY KEY,
                 `name` VARCHAR(32) NOT NULL,
                 `type` VARCHAR(16) NOT NULL,
-                `rarity` VARCHAR(8) NOT NULL,
-                `set` VARCHAR(4) NOT NULL,
-                `number` SMALLINT NOT NULL,
                 `multiverseid` INT UNIQUE NOT NULL,
                 `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             ) ENGINE=INNODB;
