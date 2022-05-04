3efa20667f76115a68b7f8dc1937e1ad3373fa54
mitchtheuvenet <mitch.theuvenet@gmail.com>
2022-05-04 14:44:30 +0200

    added MTG twitter feed to homepage

--- views/home.php
+++ views/home.php
@@ -10,4 +10,8 @@ $this->title = 'Home';
     <a href="/" tabindex="-1">
         <img src="images/logo.png" class="img-fluid" alt="Logo">
     </a>
+    <div class="my-4">
+        <a class="twitter-timeline" data-width="1024" data-height="384" data-dnt="true" href="https://twitter.com/wizards_magic?ref_src=twsrc%5Etfw">Tweets by wizards_magic</a>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
 </div>
