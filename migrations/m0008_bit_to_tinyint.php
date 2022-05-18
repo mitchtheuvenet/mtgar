dac57d5f62bb57b1ef6a175e7437cc223ebb4c24
mitchtheuvenet <mitch.theuvenet@gmail.com>
2022-05-18 18:43:58 +0200

    not nullable

--- migrations/m0008_bit_to_tinyint.php
+++ migrations/m0008_bit_to_tinyint.php
@@ -6,25 +6,25 @@ class m0008_bit_to_tinyint extends Migration {
 
     public function up() {
         self::exec(<<<'SQL'
-            ALTER TABLE `users` MODIFY `admin` TINYINT(1);
+            ALTER TABLE `users` MODIFY `admin` TINYINT(1) NOT NULL;
         SQL);
         self::exec(<<<'SQL'
-            ALTER TABLE `verifications` MODIFY `used` TINYINT(1);
+            ALTER TABLE `verifications` MODIFY `used` TINYINT(1) NOT NULL;
         SQL);
         self::exec(<<<'SQL'
-            ALTER TABLE `verifications` MODIFY `expired` TINYINT(1);
+            ALTER TABLE `verifications` MODIFY `expired` TINYINT(1) NOT NULL;
         SQL);
     }
 
     public function down() {
         self::exec(<<<'SQL'
-            ALTER TABLE `users` MODIFY `admin` BIT(1);
+            ALTER TABLE `users` MODIFY `admin` BIT(1) NOT NULL;
         SQL);
         self::exec(<<<'SQL'
-            ALTER TABLE `verifications` MODIFY `used` BIT(1);
+            ALTER TABLE `verifications` MODIFY `used` BIT(1) NOT NULL;
         SQL);
         self::exec(<<<'SQL'
-            ALTER TABLE `verifications` MODIFY `expired` BIT(1);
+            ALTER TABLE `verifications` MODIFY `expired` BIT(1) NOT NULL;
         SQL);
     }
 
